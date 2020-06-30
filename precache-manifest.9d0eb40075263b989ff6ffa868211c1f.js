f7630293f4602c3d791ea8ab81a9e5c7791d39c9
Nitesh Singh <niteshsinghnrs@gmail.com>
2020-07-01 00:30:34 +0530

    First Commit

---
+++ precache-manifest.9d0eb40075263b989ff6ffa868211c1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d8b4b235bbb689242b65d9755208bb0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b387732a38bf4c267c0b",
+    "url": "./static/css/main.42c06708.chunk.css"
+  },
+  {
+    "revision": "3593ab5afcd7a5c4a0d5",
+    "url": "./static/js/2.d5ca78ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.d5ca78ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b387732a38bf4c267c0b",
+    "url": "./static/js/main.6bd55b83.chunk.js"
+  },
+  {
+    "revision": "c6bb8d75b13076c18626",
+    "url": "./static/js/runtime-main.457fe1db.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
